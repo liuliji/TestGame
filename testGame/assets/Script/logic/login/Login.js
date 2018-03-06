285dfc81420767b9e1fcc5e2a266768dfc6ab777
刘立极 <liuliji1184899343@163.com>
2018-03-06 23:03:25 +0800

    加入common控件，添加简单的UI

---
+++ testGame/assets/Script/logic/login/Login.js
@@ -0,0 +1,30 @@
+/************************************************************************
+ * Copyright (c) 2018 testGame
+ * Author    : liji.liu
+ * Mail      : liuliji1184899343@163.com
+ * Date      : 2017-3-4
+ * Use       : Login场景管理
+ ************************************************************************/
+
+var App = require('App');
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+
+
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        if (App.Init()) { // 初始化控制器
+        }
+        App.UIManager.showAwait();
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
