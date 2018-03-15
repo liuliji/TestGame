f9888337486a7a5dc302a056280e12fd12890760
刘立极 <liuliji1184899343@163.com>
2018-03-15 22:12:01 +0800

    bug修复

--- client/testGame/assets/Script/networks/msgSend/LobbySendMsgs.js
+++ client/testGame/assets/Script/networks/msgSend/LobbySendMsgs.js
@@ -44,7 +44,7 @@ function onTalk(content) {
 }
 
 module.exports = {
-    'oncreateRoom': oncreateRoom,// 创建房间
+    'onCreateRoom': onCreateRoom,// 创建房间
     'onEnterRoom': onEnterRoom,// 加入房间
     'onDeleteRoom': onDeleteRoom,// 删除房间
     'onTalk': onTalk,// 说话、聊天
