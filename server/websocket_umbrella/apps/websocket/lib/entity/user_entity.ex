b11c5e12b8c9b63a7e2140d627dbf4714cea8f4f
jie <997269684@qq.com>
2018-03-28 18:10:36 +0800

    初步修改user模块

--- server/websocket_umbrella/apps/websocket/lib/entity/user_entity.ex
+++ server/websocket_umbrella/apps/websocket/lib/entity/user_entity.ex
@@ -7,50 +7,71 @@ defmodule Websocket.UserEntity do
 
     defmodule User do
         defstruct(
+            # 用户基本信息
             uid: "",
-            roomId: "",
             userName: "",
+            
+            # 和房间相关信息
+            roomId: "",
+            roomOwner: "",
+            position: -1,   # -1 是初始值，代表没有安排座位
+            readyStatus: false,
+
+            #游戏相关信息
+
+            # 其他重要信息
+            pid: nil,
             socketId: ""
             )
     end
 
-    def start_link(uid, userName) do
-        {:ok, pid} = Entity.start(uid, %User{uid: uid, userName: userName, socketId: uid})
+    def start_link(uid, userName, socketId) do
+        {:ok, pid} = Entity.start(uid, %{})
+        Entity.put_behaviour(pid, DefaultBehaviour, %User{uid: uid, userName: userName, socketId: socketId, pid: pid})
+        {:ok, pid}
     end
 
-    def test(pid) do
-        attr = Entity.get_attribute(pid, :userName)
-        Entity.call_behaviour(pid, DefaultBehaviour, :test_behaviour)
-        Logger.debug "file:#{inspect Path.basename(__ENV__.file)} line:#{__ENV__.line}
-        #{inspect attr}"
+    def get_info(pid) do
+        Entity.get_attribute(pid, User)
+    end
+
+    def get_attr(pid, key) do
+        Entity.get_attribute(pid, User)[key]
+    end
+
+    def update_info(pid, map) do
+        Entity.update_attribute(pid, User,
+        fn user ->
+            map |> Enum.map(fn {key, value} ->
+                user |> Map.put(key, value)
+                )
+        end)
     end
 
     defmodule DefaultBehaviour do
-        use Entity.Behaviour
+        use Entice.Entity.Behaviour
+        alias Entice.Entity
 
         # put_behaviour 会回调该behaviour的init方法
-        def init([state, args] = list) do
-            Logger.debug "file:#{inspect Path.basename(__ENV__.file)} line:#{__ENV__.line}
-            #{inspect list}"
-            {:ok, state}
+        def init(%Entity{attributes: attr} = entity, %User{pid: pid} = user) do
+
+            entity = entity |> put_attribute(user)
+
+            {:ok, %{state | attributes: attr}}
         end
 
-        def handle_call(event, state) do
+        def handle_call(event, entity) do
             Logger.debug "file:#{inspect Path.basename(__ENV__.file)} line:#{__ENV__.line}
             handle call #{inspect event}"
             {:ok, :ok, state}
         end
 
-        def handle_event(event, state) do
+        def handle_event(event, entity) do
             Logger.debug "file:#{inspect Path.basename(__ENV__.file)} line:#{__ENV__.line}
             #{inspect event}"
             {:ok, state}
         end
 
-        def test_behaviour() do
-            Logger.debug "file:#{inspect Path.basename(__ENV__.file)} line:#{__ENV__.line}
-            test behaviour"
-        end
 
     end
 
