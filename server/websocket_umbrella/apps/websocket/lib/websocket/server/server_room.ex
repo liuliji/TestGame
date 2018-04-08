ab46042eb4a5c1a641e223bca98012cda68d7650
jie <997269684@qq.com>
2018-04-08 18:09:59 +0800

    小调整

--- server/websocket_umbrella/apps/websocket/lib/websocket/server/server_room.ex
+++ server/websocket_umbrella/apps/websocket/lib/websocket/server/server_room.ex
@@ -6,13 +6,21 @@ defmodule Websocket.ServerRoom do
     alias Entice.Entity.Coordination
     alias Websocket.ServerUser.User
 
+
+    # 最完美的方案就是 数据全部都在 server_room 接收到了消息之后，开始设置。
+    # user_room只在server_room返回的消息里面更新数据，从client传递过来的消息仅仅是传递，因为并不能保证server_room能接收到消息，
+    # 如果必须要在 user_room接收client的消息里面更新用户数据，那么这种数据最好是可以重写的，就是它并不是 下一次再发起这种消息的时候的基础数据。
+    # 
+
     defmodule Room do
         defstruct(
             roomId: "",
             pid: nil,
-            users: []
+            users: [],       # 存放的user pid
+            seats: [nil, nil, nil, nil, nil]    #存放的user id
         )
     end
+
 
     def new_room(roomId) do
         {:ok, pid} = Entity.start(roomId)
@@ -92,6 +100,28 @@ defmodule Websocket.ServerRoom do
             notify_all msg:#{inspect msg}. room.users:#{inspect room.users}"
             room.users |> Enum.map(fn pid -> send(pid, msg) end)
             {:ok, entity}
+        end
+
+        @doc """
+        找到了座位 返回>0 没找到 返回-1
+        """
+        def handle_event({:takeSeat, uid}, entity) do
+            room = entity |> Entity.get_attribute(Room)
+            if (room.seats |> Enum.member?(uid)) do
+                index = room.seats |> Enum.find_index(fn x -> x==uid end)
+            else
+                {isFind, pos} = room.seats |> Enum.reduce({false, 0}, fn x, {isFind, pos} ->
+                    unless (isFind) do
+                        if (x == nil) do
+                            isFind = true
+                        else
+                            pos = pos + 1
+                        end
+                    end
+                end)
+
+                if ()
+            end
         end
     end
 
